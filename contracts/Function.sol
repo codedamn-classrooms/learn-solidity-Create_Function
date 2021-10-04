7676287c7d3ad73a9d4bbe926b688b8b6023f5ec
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2021-10-04 16:35:00 +0530

    Update Function.sol

--- contracts/Function.sol
+++ contracts/Function.sol
@@ -2,13 +2,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Function{
-    uint number=10;
-    function returnStateVariable() public view returns(uint){
-        return number;
-    }
-    function createLocalVariable() public pure returns(uint){
-        uint integer=20;
-        return integer;
-    }
-
+    
 }
